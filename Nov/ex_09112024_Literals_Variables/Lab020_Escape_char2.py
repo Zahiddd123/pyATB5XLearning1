d7e9b0d81987004a195aaebdfefa539abbe6a396
Zahiddd123 <Zahid.rabbani63@gmail.com>
2024-11-11 23:30:37 +0530

    9 Nov

---
+++ Nov/ex_09112024_Literals_Variables/Lab020_Escape_char2.py
@@ -0,0 +1,18 @@
+#Escape sequence
+
+# '' or ""
+
+c= 'C'
+c1= "C"
+
+#Where the concept r will be used?
+# dir = 'C:\Zahid\n.txt'  "To print this special char as a str we have to use r
+# dir1 = "C:\Zahid\n.txt"
+
+dir = r'C:\Zahid\n.txt'
+dir1 = r"C:\Zahid\n.txt"
+
+print(dir)
+print(type(dir))
+print(dir1)
+print(type(dir1))
